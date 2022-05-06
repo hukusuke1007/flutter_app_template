3ae0a0d78c20787f4382bd52cbb1e5c79ef89cff
ymgn9314 <yamagen.private@gmail.com>
2022-05-07 00:19:28 +0900

    improve create_keystore_debug/release command

--- sh/create_keystore_debug.sh
+++ sh/create_keystore_debug.sh
@@ -1,4 +1,14 @@
-# debug
-keytool -genkey -v -keystore android/debug.jks -alias androiddebugkey -keyalg RSA -validity 10000 -dname "CN=Android Debug,O=Android,C=JA"
-keytool -list -v -alias androiddebugkey -keystore android/debug.jks
-keytool -exportcert -alias androiddebugkey -keystore android/debug.jks | openssl sha1 -binary | openssl base64
+SCRIPT_DIR="$(dirname "$0")"
+KEY_ALIAS="androiddebugkey"
+PASSWORD="password"
+JKS_FILE="../android/debug.jks"
+
+cd $SCRIPT_DIR
+
+if [ -e $JKS_FILE ]; then
+    rm $JKS_FILE
+fi
+
+keytool -genkey -v -keystore $JKS_FILE -alias $KEY_ALIAS -storepass $PASSWORD -keypass $PASSWORD -keyalg RSA -validity 10000 -dname "CN=Android Debug,O=Android,C=JP"
+keytool -list -v -alias $KEY_ALIAS -keystore $JKS_FILE -storepass $PASSWORD
+keytool -exportcert -alias $KEY_ALIAS -storepass $PASSWORD -keystore $JKS_FILE | openssl sha1 -binary | openssl base64
