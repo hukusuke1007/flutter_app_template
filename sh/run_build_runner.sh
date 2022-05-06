ada8443b4b3d57758243ab403230550a2bc1956a
ymgn9314 <yamagen.private@gmail.com>
2022-05-07 00:05:29 +0900

    add run_build_runner_watch.sh

--- sh/run_build_runner.sh
+++ sh/run_build_runner.sh
@@ -1,2 +1,2 @@
-flutter packages pub run build_runner build
-#flutter packages pub run build_runner build --delete-conflicting-outputs
+flutter pub run build_runner build
+#flutter pub run build_runner build --delete-conflicting-outputs
