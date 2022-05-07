095b1f63a730c3e07ba0b65b457f570accdad51d
ymgn9314 <yamagen.private@gmail.com>
2022-05-07 10:24:49 +0900

    add sendEmailVerificationProvider

---
+++ lib/model/use_cases/auth/email/send_email_verification.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_app_template/model/exceptions/app_exception.dart';
+import 'package:flutter_app_template/model/repositories/firebase_auth/firebase_auth_repository.dart';
+import 'package:flutter_app_template/model/use_cases/auth/auth_state.dart';
+import 'package:flutter_app_template/utils/logger.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final sendEmailVerificationProvider =
+    Provider.autoDispose((ref) => SendEmailVerification(ref.read));
+
+class SendEmailVerification {
+  SendEmailVerification(this._read);
+  final Reader _read;
+
+  Future<void> call() async {
+    final repository = _read(firebaseAuthRepositoryProvider);
+    final controller = _read(authProvider.state);
+
+    final user = repository.authUser;
+    if (user == null) {
+      throw AppException(title: 'ログインしてください');
+    }
+
+    await repository.sendEmailVerification(user);
+    controller.update((state) => AuthState.noSignIn);
+
+    logger.info('確認メールを送信しました。');
+  }
+}
