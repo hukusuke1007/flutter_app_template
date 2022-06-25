782698a2790e98d3eea7400c0e70cf9377ebe79e
mihiro <f.mihiroxx@gmail.com>
2022-06-25 21:26:31 +0900

    Change package from package_info to package_info_plus

--- lib/model/repositories/package_info/package_info_repository.dart
+++ lib/model/repositories/package_info/package_info_repository.dart
@@ -1,5 +1,5 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 final packageInfoRepositoryProvider =
     Provider<PackageInfoRepository>((_) => throw UnimplementedError());
