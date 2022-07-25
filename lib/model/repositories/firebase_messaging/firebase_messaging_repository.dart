65cdd08e7f5b829bd0a2ad6b7a5d07c40e78c632
shohei <hukusuke1007@gmail.com>
2022-07-25 09:46:17 +0900

    Update firebase_messaging_repository.dart

--- lib/model/repositories/firebase_messaging/firebase_messaging_repository.dart
+++ lib/model/repositories/firebase_messaging/firebase_messaging_repository.dart
@@ -5,7 +5,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:rxdart/rxdart.dart';
 
-import '../../../model/repositories/firebase_messaging/topics.dart';
+import 'topics.dart';
 
 final firebaseMessagingRepositoryProvider =
     Provider<FirebaseMessagingRepository>(
