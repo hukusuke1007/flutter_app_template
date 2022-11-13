18704c1f6ab26f77e5e79536305596f4dcb3ec97
shohei <hukusuke1007@gmail.com>
2022-11-13 14:50:04 +0900

    removed android_id

--- lib/model/repositories/device_info/device_info_repository.dart
+++ lib/model/repositories/device_info/device_info_repository.dart
@@ -1,32 +1,27 @@
 import 'dart:io';
 
-import 'package:android_id/android_id.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final deviceInfoRepositoryProvider = Provider<DeviceInfoRepository>(
-  (_) => DeviceInfoRepository(
-    DeviceInfoPlugin(),
-    const AndroidId(),
-  ),
+  (_) => DeviceInfoRepository(DeviceInfoPlugin()),
 );
 
 class DeviceInfoRepository {
   DeviceInfoRepository(
     this._deviceInfo,
-    this._androidId,
   );
 
   final DeviceInfoPlugin _deviceInfo;
-  final AndroidId _androidId;
 
   Future<String?> get deviceId async {
     if (Platform.isIOS) {
       final info = await _deviceInfo.iosInfo;
       return info.identifierForVendor;
     } else if (Platform.isAndroid) {
-      final androidId = await _androidId.getId();
-      return androidId;
+      /// device_info_plusではAndroidの端末IDは取得できない
+      /// android_idパッケージを使えば取得できるがReleaseビルドで落ちるため推奨しない
+      /// 本プロジェクトではnullを返却する
     }
     return null;
   }
