fb75f71240a354badd457fa84c547a7dc8acae69
shohei <hukusuke1007@gmail.com>
2024-01-05 17:34:05 +0900

    Update button_style.dart

--- feature_first/lib/core/res/button_style.dart
+++ feature_first/lib/core/res/button_style.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 
-const kButtonMinimumHeight = 40.0;
-
 class ButtonStyles {
   ButtonStyles._();
 
   static ButtonStyle normal({
     double elevation = 2,
+    Size minimumSize = const Size(80, 40),
   }) =>
       FilledButton.styleFrom(
         elevation: elevation,
-        minimumSize: const Size(80, 40),
+        minimumSize: minimumSize,
       );
 }
